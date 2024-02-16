ccc3a513a173c73301643f709ea367ae38524e44
Surabhi-Sunil <surabhi.sunil2108@gmail.com>
2024-02-16 13:59:39 -0600

    New commit

--- src/CaseCard.js
+++ src/CaseCard.js
@@ -1,37 +1,47 @@
 // CaseCard.js
-import React from 'react';
+// eslint-disable-next-line
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
+import { Chip } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import Axios from 'axios';
 
-function CaseCard  () {
-  const dataFromDatabase = [
-    { id: 1, title: 'Lizard', content: ' ABCCC' },
-    { id: 2, title: 'Another Card', content: 'Some content for another card' },
-    // ... more data
-  ];
+
+
+function CaseCard() {
+  const [caseStudyData, setCaseStudyData] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/getCases').then((response) => {
+      console.log(response.data)
+      setCaseStudyData(response.data)
+    })
+  }, [])
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      {dataFromDatabase.map((caseStudy) => (
-        <Card key={caseStudy.id} sx={{ minWidth: 345, minHeight: 150, maxWidth: 345, margin: '10px' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', maxWidth: 'auto', overflowY: 'auto' }}>
+      {caseStudyData.map((caseStudy) => (
+        <Card key={caseStudy.caseID} sx={{ minWidth: 450, minHeight: 150, maxWidth: 345, margin: '10px' }}>
           <CardActionArea>
+            {caseStudy.Image && (
+              <img style={{width:"inherit", height:"inherit"}} src={require(`${caseStudy.Image}`)} alt="logo" />
+            )}
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                {caseStudy.title}
+                {caseStudy.CaseTitle}
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                {caseStudy.content}
+                {caseStudy.Complaint}
               </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
+            {caseStudy.tags && caseStudy.tags.map((tag, index) => (
+              <Chip key={index} label={tag} />
+            ))}
           </CardActions>
         </Card>
       ))}
